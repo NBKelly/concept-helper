c33dd88cbe5cf4f8532b62b54cf5c12b300ec9fe
NBKelly <nbk4@students.waikato.ac.nz>
2020-05-08 17:28:58 +1200

    I think all the kinks are worked out now

--- rename.sh
+++ rename.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]
-  then
-      echo "No arguments supplied"
-      exit
+then
+    echo "No arguments supplied"
+    exit
 fi
+
+SCRIPDIR=`dirname "$0"`/
+CURDIR=`pwd`
+cd $SCRIPDIR
+SCRIPDIR=`pwd`
+cd $CURDIR
+
+echo "SCRIPDIR: "$SCRIPDIR
+
+if [ $# -eq 1 ]
+then
+    #we need to figure out where the script is being run from,
+    #so we can deposite the created files there and reference file
+    echo "placing output files in current directory"
+    OUTDIR=`pwd`
+fi
+
+#second argument makes a folder (at the cd), and places the files in there
+if [ $# -eq 2 ]
+then
+    #we're giving a specific output directory
+    #let's get the full qualified name of that directory
+    echo "making directory $2, and placing output files there..."
+    mkdir $2
+    cd $2
+    OUTDIR=`pwd`
+    
+fi
+
+cd $SCRIPDIR
+pwd
+echo "OUTDIR: "$OUTDIR
 
 ##first replacement: we want to make a file called conceptHelper.java, which is our superclass
 
@@ -22,3 +54,7 @@ sed "s/myHelperClass/${1}/g" myHelperClass.java > $1.javatmp
 #replace superclass mentions
 sed "s/myLibTemplate/${CONCEPTSTR}/g" $1.javatmp > $1.java
 
+rm $1.javatmp
+mv $1.java $OUTDIR
+mv $CONCEPTSTR.java $OUTDIR
+cp DebugLogger.java $OUTDIR
