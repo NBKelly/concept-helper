48aac65e05ffbcd0652a011be6bf4f1828a93c61
NBKelly <nbk4@students.waikato.ac.nz>
2020-05-08 19:50:57 +1200

    add example, add documentation

--- myHelperClass.java
+++ myHelperClass.java
@@ -1,6 +1,48 @@
 import java.util.Scanner;
 
 public class myHelperClass extends myLibTemplate {
+    //BE AWARE OF:
+    //  DEBUG, IGNORE UNCLEAN, TIMER, DEBUG_TIME_MAGNITUDE
+    //  the timer and debug functions make use of these
+
+    // to pass a logger to another class:
+    //   getDebugLogger()  :  seeDebugLogger.java
+
+    // Input:
+    //   * NOTE: checking must be explicit (existence is assumed)
+    
+    //   hasNextInt()        -> boolean, input contains another int
+    //   hasNextBigInteger() -> boolean, same as above but bigint
+    //   hasNextLine()       -> boolean, same as above but String (full line)
+    //   nextInt()           -> integer, gets next int from stream
+    //   nextBigInteger()    -> bigint,  gets next bigint from stream
+    //   nextLine()          -> String,  gets all text from stream up to next line break
+    //  
+    
+    // To print:
+    //   Errors: ERR(x)       -> uses stderr, only enabled with -se
+    //   Line:   println(x)
+    //   Block:  print(x)     -> inserts space after integer argument
+    //   Debug:  DEBUG(x)     -> uses stderr, only enabled with -d
+    //   Debug:  DEBUGF(x,[]) -> uses stderr, only enabled with -d
+    //   Timer:  TEBUG(x)     -> uses stderr, enabled with -d/-t
+    //   Timer:  TEBUGF(x,[]) -> uses stderr, enabled with -d/-t
+    //
+    // These take input of arbitrary object, or integer
+    // V(object) = object.toString();
+
+    // Timer:
+    // * start()             -> gets time, returns timer too
+    //   split()             -> split(null)
+    // * split(Str)          -> prints split time, and also reason
+    // * total(Str, bool)    -> prints total time, reason, bool = reset
+    //   total()             -> total(null, false)
+    //   total(Str)          -> total(Str, false)
+    //   total(bool)         -> total(null, bool)
+    //   reset()             -> resets timer
+    
+    // GCD, LCM:
+    //   Single pairs, or lists of numbers are supported <>, []
     public myHelperClass() {
 	super();
     }
@@ -37,7 +79,16 @@ public class myHelperClass extends myLibTemplate {
 	return true;
     }
 
+    /* ^^^^ NOT YOUR WORK
+     *
+     *     YOUR WORK HERE
+     * 
+     * vvvv YOUR WORk
+     */
+    
     public void solveProblem() throws Exception {
+	System.out.println("Hello, world!");
+	
 	/**
 	 *  Timer t = new Timer().start();
 	 *  
@@ -47,7 +98,7 @@ public class myHelperClass extends myLibTemplate {
 	 *      //your logic here
 	 *  }
 	 *  
-	 *  t.total("Finished processing of file. 
+	 *  t.total("Finished processing of file. ");
 	 */
     }
 }
