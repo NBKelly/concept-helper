2120cf16e1036d78d7c8b3a228410ca3c863f9e8
NBKelly <nbk4@students.waikato.ac.nz>
2020-05-15 10:38:19 +1200

    easy.sh works again

--- easy.sh
+++ easy.sh
@@ -118,4 +118,4 @@ AUXPACKAGE=${AUXPACKAGE%.}
 #echo $PACKAGE
 #echo $AUXPACKAGE
 #$TARGET
-echo $TARGET -c "$classname" -d "$location" -p "$PACKAGE" -s "$AUXPACKAGE" -t "$auxdir"
+$TARGET -c "$classname" -d "$location" -p "$PACKAGE" -s "$AUXPACKAGE" -t "$auxdir"
